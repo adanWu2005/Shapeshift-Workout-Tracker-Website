a54b5e8583dd742366694a3844f79efd4ba8a04c
Adan Wu <162506142+adanWu2005@users.noreply.github.com>
2024-10-08 16:30:57 -0400

    Add files via upload

---
+++ src/assets/workoutDataBack.js
@@ -0,0 +1,82 @@
+import img1 from '../assets/BackImages/dl.jpg'
+import img2 from '../assets/BackImages/bentO.jpg'
+import img3 from '../assets/BackImages/pullU.jpg'
+import img4 from '../assets/BackImages/1row.png'
+import img5 from '../assets/BackImages/2row.webp'
+import img6 from '../assets/BackImages/latP.webp'
+import img7 from '../assets/BackImages/tbar.jpg'
+import img8 from '../assets/BackImages/srow.jpg'
+import img9 from '../assets/BackImages/saPd.webp'
+import img10 from '../assets/BackImages/fp.jpg'
+import img11 from '../assets/BackImages/bext.jpg'
+
+const Workout_Back_Data = [
+    {
+        id:1,
+        name: "Barbell deadlift",
+        category: "back",
+        img: img1
+    },
+    {
+        id:2,
+        name: "Barbell bent-over row",
+        category: "back",
+        img: img2
+    },
+    {
+        id:3,
+        name: "Pullups",
+        category: "back",
+        img: img3
+    },
+    {
+        id:4,
+        name: "Dumbbell single-arm row",
+        category: "back",
+        img: img4
+    },
+    {
+        id:5,
+        name: "Dumbbell row",
+        category: "back",
+        img: img5
+    },
+    {
+        id:6,
+        name: "Lat pulldowns",
+        category: "back",
+        img: img6
+    },
+    {
+        id:7,
+        name: "T-Bar row",
+        category: "back",
+        img: img7
+    },
+    {
+        id:8,
+        name: "Seated Row",
+        category: "back",
+        img: img8
+    },
+    {
+        id:9,
+        name: "Striaght-arm pulldown",
+        category: "back",
+        img: img9
+    },
+    {
+        id:10,
+        name: "Face pulls",
+        category: "back",
+        img: img10
+    },
+    {
+        id:11,
+        name: "Back extensions",
+        category: "back",
+        img: img11
+    },
+]
+
+export default Workout_Back_Data
